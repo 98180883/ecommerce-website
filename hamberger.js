f693e2f76e971dd78b68b5ce184f512b0331c0ce
Dipanjan Karmakar <www.sk1748@gmail.com>
2026-03-06 17:21:18 +0530

    header layout bug fix

--- hamberger.js
+++ hamberger.js
@@ -1,8 +1,14 @@
-/*menu mobile*/
-
 const menuBtn = document.getElementById("menu-btn");
 const nav = document.querySelector(".mob-nav");
 
-menuBtn.addEventListener("click" , () => {
+menuBtn.addEventListener("click", () => {
     nav.classList.toggle("open");
-})
+
+    if (nav.classList.contains("open")) {
+        document.body.style.overflow = "hidden"; // body won't scroll
+        menuBtn.innerHTML="✕";
+    } else {
+         menuBtn.innerHTML="☰";
+        document.body.style.overflow = ""; // restore default
+    }
+});
